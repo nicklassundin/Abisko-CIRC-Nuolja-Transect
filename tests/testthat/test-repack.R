1cbf6290ea04f5b4d4733a8beaa4dd2aa8707b20
nicklassundin <p4cr@hotmail.com>
2024-04-29 16:16:29 +0200

    local save

--- tests/testthat/test-repack.R
+++ tests/testthat/test-repack.R
@@ -43,11 +43,35 @@ test_that("noneNum", {
 	expect_that(result, equals(answer));
 })
 
-context("Readfile");
-test_that("readfile", {
-	path <- getPaths("/data");
-	path <- getDataFilesPaths(path)[1];
-	file <- readFile(path);
-	file <- delist(file[1,2])
-	expect_that(file, equals("2018-05-10"));
+# context("Readfile");
+# test_that("readfile", {
+# 	path <- getPaths("/data");
+# 	path <- getDataFilesPaths(path)[1];
+# 	file <- readFile(path);
+# 	file <- delist(file[1,2])
+# 	# expect_that(file, equals("2018-05-10"));
+# })
+
+dates <- c("S-20200724", "S-20200724-001Q", "S-20200724-002", "NS-20200724-003", "NS20200724-004");
+answer <- c("20200724", "20200724", "20200724", "20200724", "20200724");
+context("format dates")
+test_that(paste("case 1:", dates[1]), {
+	result <- formatDate(dates[1]);
+	expect_that(result, equals(as.Date(answer[1], "%Y%m%d")));
+})
+test_that(paste("case 2:", dates[2]), {
+	result <- formatDate(dates[2]);
+	expect_that(result, equals(as.Date(answer[2], "%Y%m%d")));
+})
+test_that(paste("case 3:", dates[3]), {
+	result <- formatDate(dates[3]);
+	expect_that(result, equals(as.Date(answer[3], "%Y%m%d")));
+})
+test_that(paste("case 4:", dates[4]), {
+	result <- formatDate(dates[4]);
+	expect_that(result, equals(as.Date(answer[4], "%Y%m%d")));
+})
+test_that(paste("case 5:", dates[5]), {
+	result <- formatDate(dates[5]);
+	expect_that(result, equals(as.Date(answer[5], "%Y%m%d")));
 })
